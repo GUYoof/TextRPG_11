7925ddd35ddc4c31bac414b5f3d474dd4c377a14
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-04-22 17:01:14 +0900

    상점 틀린번호입력시 수정
    break; 추가

--- shop.cs
+++ shop.cs
@@ -129,6 +129,7 @@ namespace TXT11
                         else
                         {
                             Console.WriteLine("올바른 번호를 입력해주세요.");
+                            break;
                         }
                         Console.WriteLine("숫자를 입력해주세요.");
 
@@ -221,12 +222,8 @@ namespace TXT11
                             break;
                     }
                     Console.WriteLine("숫자를 입력해주세요.");
-                    Console.ReadLine();
                 }
-
-
                 else Console.WriteLine("Gold가 부족합니다.");
-                Console.ReadLine();
             }
         }
     }
